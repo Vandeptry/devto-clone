edfb5f824d9a7bfa86f417d9231d8c7a9aae87a0
Vandeptry <johnevanjr1902@gmail.com>
2024-12-21 01:42:37 +0700

    new components

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -48,11 +48,17 @@ const useFormField = () => {
   };
 };
 
-const FormItem = ({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLDivElement>) => (
+const FormItem = ({
+    children,
+    ...props
+  }: {
+    children: React.ReactNode;
+  } & React.HTMLAttributes<HTMLDivElement>) => (
     <div className="space-y-1" {...props}>
       {children}
     </div>
   );
+  FormItem.displayName = "FormItem";
   
   const FormLabel = ({ ...props }) => {
     const { error } = useFormField();
@@ -66,32 +72,47 @@ const FormItem = ({ children, ...props }: { children: React.ReactNode } & React.
       />
     );
   };
+  FormLabel.displayName = "FormLabel";
   
   const FormControl = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ ...props }, ref) => {
-  const { error } = useFormField();
-  return (
-    <input
-      ref={ref}
-      className={cn(
-        "block w-full rounded-md border-0 py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
-        error && "ring-red-500"
-      )}
+    HTMLInputElement,
+    React.InputHTMLAttributes<HTMLInputElement>
+  >(({ ...props }, ref) => {
+    const { error } = useFormField();
+    return (
+      <input
+        ref={ref}
+        className={cn(
+          "block w-full rounded-md border-0 py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
+          error && "ring-red-500"
+        )}
+        {...props}
+      />
+    );
+  });
+  FormControl.displayName = "FormControl";
+  
+  const Button = ({
+    children,
+    ...props
+  }: {
+    children: React.ReactNode;
+  } & React.ButtonHTMLAttributes<HTMLButtonElement>) => (
+    <button
+      className="w-full rounded-md bg-indigo-600 px-3 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-5"
       {...props}
-    />
+    >
+      {children}
+    </button>
   );
-});
-
-const Button = ({ children, ...props }: { children: React.ReactNode } & React.ButtonHTMLAttributes<HTMLButtonElement>) => (
-  <button
-    className="w-full rounded-md bg-indigo-600 px-3 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-5"
-    {...props}
-  >
-    {children}
-  </button>
-);
-Button.displayName = 'Button';
-
-export { useFormField, Form, FormItem, FormLabel, FormControl, FormField,Button };
+  Button.displayName = "Button";
+  
+  export {
+    useFormField,
+    Form,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormField,
+    Button,
+  };
